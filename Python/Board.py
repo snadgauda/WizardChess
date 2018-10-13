2586524baf83eb64e9ff8dd4bd6534eae1f12b69
Aditya Khant <33049089+aditya-khant@users.noreply.github.com>
2018-10-13 03:49:55 -0700

    added gitignore

--- Python/Board.py
+++ Python/Board.py
@@ -1,31 +1,31 @@
-import Piece
-import pyserial
+import  Piece.Piece
+import serial
 
 class Board:
-    def __init__(self, chessBoard):
-        blankPiece = new Piece()
+    def __init__(self):
+        blankPiece =  Piece.Piece()
         self.chessBoard = [10*[8*[blankPiece]]]
-        self.chessBoard[1][0] = new Piece("WHITE", "ROOK")
-        self.chessBoard[2][0] = new Piece("WHITE", "KNIGHT")
-        self.chessBoard[3][0] = new Piece("WHITE", "BISHOP")
-        self.chessBoard[4][0] = new Piece("WHITE", "QUEEN")
-        self.chessBoard[5][0] = new Piece("WHITE", "KING")
-        self.chessBoard[6][0] = new Piece("WHITE", "BISHOP")
-        self.chessBoard[7][0] = new Piece("WHITE", "KNIGHT")
-        self.chessBoard[8][0] = new Piece("WHITE", "ROOK")
-        whitePawn = new Piece("WHITE", "PAWN")
+        self.chessBoard[1][0] =  Piece.Piece("WHITE", "ROOK")
+        self.chessBoard[2][0] =  Piece.Piece("WHITE", "KNIGHT")
+        self.chessBoard[3][0] =  Piece.Piece("WHITE", "BISHOP")
+        self.chessBoard[4][0] =  Piece.Piece("WHITE", "QUEEN")
+        self.chessBoard[5][0] =  Piece.Piece("WHITE", "KING")
+        self.chessBoard[6][0] =  Piece.Piece("WHITE", "BISHOP")
+        self.chessBoard[7][0] =  Piece.Piece("WHITE", "KNIGHT")
+        self.chessBoard[8][0] =  Piece.Piece("WHITE", "ROOK")
+        whitePawn =  Piece.Piece("WHITE", "PAWN")
         for i in range(1,9):
             self.chessBoard[i][1] = whitePawn
 
-        self.chessBoard[1][0] = new Piece("BLACK", "ROOK")
-        self.chessBoard[2][7] = new Piece("BLACK", "KNIGHT")
-        self.chessBoard[3][7] = new Piece("BLACK", "BISHOP")
-        self.chessBoard[4][7] = new Piece("BLACK", "QUEEN")
-        self.chessBoard[5][7] = new Piece("BLACK", "KING")
-        self.chessBoard[6][7] = new Piece("BLACK", "BISHOP")
-        self.chessBoard[7][7] = new Piece("BLACK", "KNIGHT")
-        self.chessBoard[8][7] = new Piece("BLACK", "ROOK")
-        blackPawn = new Piece("BLACK", "PAWN")
+        self.chessBoard[1][0] =  Piece.Piece("BLACK", "ROOK")
+        self.chessBoard[2][7] =  Piece.Piece("BLACK", "KNIGHT")
+        self.chessBoard[3][7] =  Piece.Piece("BLACK", "BISHOP")
+        self.chessBoard[4][7] =  Piece.Piece("BLACK", "QUEEN")
+        self.chessBoard[5][7] =  Piece.Piece("BLACK", "KING")
+        self.chessBoard[6][7] =  Piece.Piece("BLACK", "BISHOP")
+        self.chessBoard[7][7] =  Piece.Piece("BLACK", "KNIGHT")
+        self.chessBoard[8][7] =  Piece.Piece("BLACK", "ROOK")
+        blackPawn =  Piece.Piece("BLACK", "PAWN")
         for i in range(1,9):
             self.chessBoard[i][6] = blackPawn
 

e6ea86978f46e1b40415830c75623ccce999e6a1
snadgauda <33081418+snadgauda@users.noreply.github.com>
2018-10-13 03:31:32 -0700

    Piece with methods
    Added getters and setters

--- Python/Piece.py
+++ Python/Piece.py
@@ -1,8 +1,31 @@
 
 class Piece:
-    def __init__(self, team, type):
+
+    def __init__(self, team = "EMPTY", name = "EMPTY"):
         self.team = team
-        self.type = type
+        self.name = name
+    
+    def setTeam(self, newTeam):
+        self.team = newTeam 
+    
+    def setType(self, newName):
+        self.name = newName
+    
+    def isBlack(self):
+        return self.team == "BLACK"
+    
+    def isWhite(self):
+        return self.team == "WHITE"
+    
+    def isBlank(self):
+        return self.team == "EMPTY"
+    
+    def isKnite(self):
+        return self.name == "KNIGHT"
+    
+    def getName(self):
+        return self.name
+
 
 
 

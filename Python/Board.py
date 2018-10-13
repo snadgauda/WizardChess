fe23149286d63d7ad54ebc583d7f6ac54b19c7bf
David <dflores0818@gmail.com>
2018-10-13 04:29:32 -0700

    Filling out the movePiece method, added some more stubs and calls to the stub methods.

--- Python/Board.py
+++ Python/Board.py
@@ -5,38 +5,48 @@ class Board:
     def __init__(self):
         blankPiece =  Piece.Piece()
         self.chessBoard = [10*[8*[blankPiece]]]
-        self.chessBoard[1][0] =  Piece.Piece("WHITE", "ROOK")
-        self.chessBoard[2][0] =  Piece.Piece("WHITE", "KNIGHT")
-        self.chessBoard[3][0] =  Piece.Piece("WHITE", "BISHOP")
-        self.chessBoard[4][0] =  Piece.Piece("WHITE", "QUEEN")
-        self.chessBoard[5][0] =  Piece.Piece("WHITE", "KING")
-        self.chessBoard[6][0] =  Piece.Piece("WHITE", "BISHOP")
-        self.chessBoard[7][0] =  Piece.Piece("WHITE", "KNIGHT")
-        self.chessBoard[8][0] =  Piece.Piece("WHITE", "ROOK")
-        whitePawn =  Piece.Piece("WHITE", "PAWN")
+        self.chessBoard[1][0] = Piece.Piece("WHITE", "ROOK")
+        self.chessBoard[2][0] = Piece.Piece("WHITE", "KNIGHT")
+        self.chessBoard[3][0] = Piece.Piece("WHITE", "BISHOP")
+        self.chessBoard[4][0] = Piece.Piece("WHITE", "QUEEN")
+        self.chessBoard[5][0] = Piece.Piece("WHITE", "KING")
+        self.chessBoard[6][0] = Piece.Piece("WHITE", "BISHOP")
+        self.chessBoard[7][0] = Piece.Piece("WHITE", "KNIGHT")
+        self.chessBoard[8][0] = Piece.Piece("WHITE", "ROOK")
+        whitePawn = Piece.Piece("WHITE", "PAWN")
         for i in range(1,9):
             self.chessBoard[i][1] = whitePawn
 
-        self.chessBoard[1][0] =  Piece.Piece("BLACK", "ROOK")
-        self.chessBoard[2][7] =  Piece.Piece("BLACK", "KNIGHT")
-        self.chessBoard[3][7] =  Piece.Piece("BLACK", "BISHOP")
-        self.chessBoard[4][7] =  Piece.Piece("BLACK", "QUEEN")
-        self.chessBoard[5][7] =  Piece.Piece("BLACK", "KING")
-        self.chessBoard[6][7] =  Piece.Piece("BLACK", "BISHOP")
-        self.chessBoard[7][7] =  Piece.Piece("BLACK", "KNIGHT")
-        self.chessBoard[8][7] =  Piece.Piece("BLACK", "ROOK")
-        blackPawn =  Piece.Piece("BLACK", "PAWN")
+        self.chessBoard[1][0] = Piece.Piece("BLACK", "ROOK")
+        self.chessBoard[2][7] = Piece.Piece("BLACK", "KNIGHT")
+        self.chessBoard[3][7] = Piece.Piece("BLACK", "BISHOP")
+        self.chessBoard[4][7] = Piece.Piece("BLACK", "QUEEN")
+        self.chessBoard[5][7] = Piece.Piece("BLACK", "KING")
+        self.chessBoard[6][7] = Piece.Piece("BLACK", "BISHOP")
+        self.chessBoard[7][7] = Piece.Piece("BLACK", "KNIGHT")
+        self.chessBoard[8][7] = Piece.Piece("BLACK", "ROOK")
+        blackPawn = Piece.Piece("BLACK", "PAWN")
         for i in range(1,9):
             self.chessBoard[i][6] = blackPawn
 
     def movePiece(self, oldX, oldY, newX, newY):
         if not self.chessBoard[newX][newY].isBlank():
-            pass # move to queue
+            # there is something to capture, move it out of the way
+            self.moveToQueue(newX, newY)
 
         if self.chessBoard[oldX][oldY].isKnight():
             pass # call the move knight method
         else:
-            pass # just straight up move da piece.
+            # just straight up move da piece.
+            self.writeToSerial(oldX, oldY, newX, newY)
+
+        # update the board representation
+        self.chessBoard[newX][newY] = self.chessBoard[oldX][oldY]
+        self.chessBoard[oldX][oldY] = Piece.Piece();
+
+
+    def moveToQueue(self, x, y):
+        pass # breadth first search to find a path to the queue.
 
     def initSerial(self, port, baudRate = 9600):
         self.ser = serial.Serial(port, baudRate)
